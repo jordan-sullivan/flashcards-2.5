c02f6ecf1de14e8daf4192150f475d6018ae4302
Jordan Sullivan <helloandhi.jp@gmail.com>
2022-05-12 14:01:42 -0600

    fix Round class to allow to pass multiple cards

--- test/Game-test.js
+++ test/Game-test.js
@@ -1,22 +1,22 @@
-// const chai = require("chai");
-// const expect = chai.expect;
+const chai = require("chai");
+const expect = chai.expect;
 
-// const Turn = require("../src/Turn");
-// const Card = require("../src/Card");
-// const Deck = require("../src/Deck");
-// const Round = require("../src/Round");
-// const Game = require("../src/Game");
+const Turn = require("../src/Turn");
+const Card = require("../src/Card");
+const Deck = require("../src/Deck");
+const Round = require("../src/Round");
+const Game = require("../src/Game");
 
-// describe("Game", () => {
-//   let game;
+describe("Game", () => {
+  let game;
 
-//   it("should be a function", () => {
-//     expect(Game).to.be.a("function");
-//   });
+  it("should be a function", () => {
+    expect(Game).to.be.a("function");
+  });
 
-//   it("should be an instance of Game", () => {
-//     expect(game).to.be.an.instanceof(Game);
-//   });
+  it("should be an instance of Game", () => {
+    expect(game).to.be.an.instanceof(Game);
+  });
 
   
-// });
+});
