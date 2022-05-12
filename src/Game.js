3a83d1cb1d2ac1365f964ce92e2d04a0a05af46c
Jordan Sullivan <helloandhi.jp@gmail.com>
2022-05-11 23:28:28 -0600

    add Game class properties and start method

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,26 @@
+const Card = require('./Card');
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
 
 class Game {
-  constructor() {}
+  constructor() {
+    this.cards;
+    this.deck;
+    this.round;
+  }
+  start() {
+    const cards = prototypeQuestions.map((card) => {
+      return new Card(card.id, card.question, card.answers, card.correctAnswer);
+      this.cards = cards;
+      const deck = new Deck(cards);
+      this.deck = deck;
+      const round = new Round(deck);
+      this.round = round;
+      this.printMessage(deck, round);
+      this.printQuestion(round);
+    })
+  }
 
   printMessage(deck, round) {
     console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
